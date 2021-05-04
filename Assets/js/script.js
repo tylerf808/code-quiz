13de40f2d72047058187da634061b1ae3dcea2b1
Tyler Fabian <tylerf808@gmail.com>
2021-05-04 14:57:06 -0400

    Changed start function to genQuestion because it will be used for generating all quesitons

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,12 +4,9 @@ const display = document.getElementById('question');
 //Array of possible questions
 const questions = ['Question 1', 'question 2', 'question 3', 'question 4'];
 
-const start = () => {
+//Function that generates a random question from the question array
+const genQuestion = () => {
     const randomNum = Math.floor((Math.random() * 4));
     display.innerHTML = questions[randomNum];
     questions.pop(randomNum);
-}
-
-const nextQuestion = () => {
-
 }
